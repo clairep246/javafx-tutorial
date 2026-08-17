13de9482b74793aef2038c5fa666e0cf7274439b
claire p <clairephay@gmail.com>
2026-08-17 21:04:18 +0800

    Added styling for dialogue box and profile pic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,9 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-
-
-
 
 public class Main extends Application {
 
@@ -22,6 +19,7 @@ public class Main extends Application {
     
     private Image userImage = new Image(this.getClass().getResourceAsStream("/images/mountain.jpg"));
     private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/night.jpg"));
+
     @Override
     public void start(Stage stage) {
          //Setting up required components
@@ -41,9 +39,38 @@ public class Main extends Application {
          mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
 
          scene = new Scene(mainLayout);
-
          stage.setScene(scene);
          stage.show();
+
+          //Formatting the window to look as expected
+
+        stage.setTitle("Duke");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput, 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
 
          //More code to be added here later
     }
