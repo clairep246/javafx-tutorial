788b38f47105d794662addd66ea9372d92d992c1
claire p <clairephay@gmail.com>
2026-08-24 23:17:40 +0800

    Add dialagbox class and user images

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 
 public class Main extends Application {
 
@@ -16,25 +16,34 @@ public class Main extends Application {
     private Button sendButton;
     private Scene scene;
 
+    private Image userImage = new Image(
+            this.getClass().getResourceAsStream("/images/DaDuke.png"));
+
+    private Image dukeImage = new Image(
+            this.getClass().getResourceAsStream("/images/DaUser.png"));
+
     @Override
     public void start(Stage stage) {
-         //Setting up required components
+        // Setting up required components
 
-         scrollPane = new ScrollPane();
-         dialogContainer = new VBox();
-         scrollPane.setContent(dialogContainer);
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
 
-         userInput = new TextField();
-         sendButton = new Button("Send");
+        userInput = new TextField();
+        sendButton = new Button("Send");
 
-         AnchorPane mainLayout = new AnchorPane();
-         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+        DialogBox dialogBox = new DialogBox("Hello!", userImage);
+        dialogContainer.getChildren().addAll(dialogBox);
 
-         scene = new Scene(mainLayout);
+        AnchorPane mainLayout = new AnchorPane();
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
 
-         stage.setScene(scene);
-         stage.show();
+        scene = new Scene(mainLayout);
 
-         //More code to be added here later
+        stage.setScene(scene);
+        stage.show();
+
+        // More code to be added here later
     }
 }
